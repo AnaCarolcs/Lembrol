32224c83e92403f96efa2efab846f774caa15eb1
Ana Carolina <anacarolcarvalho23@gmail.com>
2017-08-10 18:30:52 -0400

    [REFACTOR] Package creation: model and view -Refactor MainActivity to View

--- Lembrol/app/src/main/java/com/example/ana/lembrol/View/MainActivity.java
+++ Lembrol/app/src/main/java/com/example/ana/lembrol/View/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.ana.lembrol;
+package com.example.ana.lembrol.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.ana.lembrol.R;
 
 public class MainActivity extends AppCompatActivity {
 
