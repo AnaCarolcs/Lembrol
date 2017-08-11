c8b7c1d563cceacd0502ff7bfd6705a75ab9a4b7
Ana Carolina <anacarolcarvalho23@gmail.com>
2017-08-11 11:45:56 -0400

    [LAYOUT] Layout LembrolListActivity -activity_lembrol_list.xml completed

--- Lembrol/app/src/main/java/com/example/ana/lembrol/View/LembrolListActivity.java
+++ Lembrol/app/src/main/java/com/example/ana/lembrol/View/LembrolListActivity.java
@@ -1,4 +1,4 @@
-package com.example.ana.lembrol;
+package com.example.ana.lembrol.View;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,12 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.example.ana.lembrol.R;
+
 public class LembrolListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lembrol_list);
+
+        
 
 
     }
--- Lembrol/app/src/main/java/com/example/ana/lembrol/View/MainActivity.java
+++ Lembrol/app/src/main/java/com/example/ana/lembrol/View/MainActivity.java
@@ -1,10 +1,12 @@
-package com.example.ana.lembrol;
+package com.example.ana.lembrol.View;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.ana.lembrol.R;
 
 public class MainActivity extends AppCompatActivity {
 
