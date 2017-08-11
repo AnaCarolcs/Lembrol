fa3641bf277f46e9514ea2e99ccad34ec26eded2
Ana Carolina <anacarolcarvalho23@gmail.com>
2017-08-11 11:25:16 -0400

    [FIRST ACTIVITY] MainActivity OK -Button MainActivity go to LembrolListActivity

---
+++ Lembrol/app/src/main/java/com/example/ana/lembrol/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.ana.lembrol;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonSignId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonSignId = (Button) findViewById(R.id.signId);
+
+        buttonSignId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, LembrolListActivity.class));
+            }
+        });
+
+    }
+}
