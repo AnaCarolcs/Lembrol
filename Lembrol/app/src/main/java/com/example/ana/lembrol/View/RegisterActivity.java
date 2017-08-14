67b117229cc1df217ec6f8d5db113c32ca483dbe
Ana Carolina <anacarolcarvalho23@gmail.com>
2017-08-14 15:29:41 -0400

    [REGISTER] registerUser method -The user can be registered in the data base

--- Lembrol/app/src/main/java/com/example/ana/lembrol/View/RegisterActivity.java
+++ Lembrol/app/src/main/java/com/example/ana/lembrol/View/RegisterActivity.java
@@ -1,14 +1,27 @@
 package com.example.ana.lembrol.View;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.ana.lembrol.Config.Base64Custom;
+import com.example.ana.lembrol.Config.Preference;
 import com.example.ana.lembrol.Model.User;
 import com.example.ana.lembrol.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+
+import com.example.ana.lembrol.Config.FirebaseConfig;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -40,11 +53,59 @@ public class RegisterActivity extends AppCompatActivity {
                 user.setName( name.getText().toString());
                 user.setEmail(email.getText().toString());
                 user.setPassword(password.getText().toString());
-                //registerUser();
+                registerUser();
 
             }
         });
     }
+
+    private void registerUser(){
+
+        authentication = FirebaseConfig.getAuthenticationFirebase();
+        authentication.createUserWithEmailAndPassword(user.getEmail(), user.getPassword())
+                .addOnCompleteListener(RegisterActivity.this,
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(RegisterActivity.this, "Cadastro efetuado com sucesso",
+                                            Toast.LENGTH_LONG).show();
+
+                                    FirebaseUser userFirebase =task.getResult().getUser();
+
+                                    String userIdentifier = Base64Custom.code64Base(user.getEmail());
+                                    user.setId(userIdentifier);
+                                    user.save();
+
+                                    Preference preference = new Preference(RegisterActivity.this);
+                                    preference.dataSave(userIdentifier);
+
+                                    //openUserLogin();
+
+                                } else {
+
+                                    try{
+                                        throw task.getException();
+
+                                    } catch (FirebaseAuthWeakPasswordException e) {
+                                        erroException = "Digite uma senha mais forte, contendo mais caracteres e com letras e números";
+                                    } catch (FirebaseAuthInvalidCredentialsException e){
+                                        erroException = "O email informado é invalido";
+                                    } catch (FirebaseAuthUserCollisionException e){
+                                        erroException = "Esse email já está cadastrado";
+                                    } catch (Exception e){
+                                        erroException = "Erro ao efetuar o cadastro";
+                                    }
+
+                                    Toast.makeText(RegisterActivity.this, "Erro: " + erroException,
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+    }
+
+
 
     }
 
