557c9270ea065c10b19e954cb642d0eb21aa6258
Ana Carolina <anacarolcarvalho23@gmail.com>
2017-08-14 17:28:43 -0400

    [VALIDATES] Validates in Login -validateUserLoggerIn (Checks if user is logged in) -validateLogin (Checks email and password)

--- Lembrol/app/src/main/java/com/example/ana/lembrol/View/LoginActivity.java
+++ Lembrol/app/src/main/java/com/example/ana/lembrol/View/LoginActivity.java
@@ -1,15 +1,22 @@
 package com.example.ana.lembrol.View;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.ana.lembrol.Config.Base64Custom;
+import com.example.ana.lembrol.Config.Preference;
 import com.example.ana.lembrol.R;
 import com.example.ana.lembrol.Config.FirebaseConfig;
 import com.example.ana.lembrol.Model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
@@ -26,7 +33,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //validateUserLoggerIn();
+        validateUserLoggerIn();
 
         email = (EditText) findViewById(R.id.loginEmailId);
         password = (EditText) findViewById(R.id.loginPasswordId);
@@ -40,7 +47,7 @@ public class LoginActivity extends AppCompatActivity {
                 user.setEmail(email.getText().toString());
                 user.setPassword(password.getText().toString());
 
-                //validateLogin();
+                validateLogin();
 
             }
 
@@ -48,14 +55,44 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-   // private  void validateUserLoggerIn(){
+    private  void validateUserLoggerIn(){
 
-   //     authentication = FirebaseConfig.getAuthenticationFirebase();
-   //     if (authentication.getCurrentUser() != null){
-   //         openMainActivity();
-   //     }
+        authentication = FirebaseConfig.getAuthenticationFirebase();
+        if (authentication.getCurrentUser() != null){
+            openMainActivity();
+        }
 
-  //  }
+    }
+
+    public void validateLogin(){
+
+        authentication = FirebaseConfig.getAuthenticationFirebase();
+        authentication.signInWithEmailAndPassword( user.getEmail(), user.getPassword())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+
+                            Preference preference = new Preference(LoginActivity.this);
+                            String userIdentifier = Base64Custom.code64Base(user.getEmail());
+                            preference.dataSave(userIdentifier);
+
+                            openMainActivity();
+
+                            Toast.makeText(LoginActivity.this, "Sucesso ao fazer Login",
+                                    Toast.LENGTH_LONG).show();
+
+                        }else{
+
+                            Toast.makeText(LoginActivity.this, "Erro ao fazer login",
+                                    Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+
+    }
 
     public void openMainActivity(){
 
