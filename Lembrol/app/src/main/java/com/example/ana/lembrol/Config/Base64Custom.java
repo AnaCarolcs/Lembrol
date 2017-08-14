7f92833bbdac33e9183840df87b7c4b5693eb12a
Ana Carolina <anacarolcarvalho23@gmail.com>
2017-08-14 15:24:22 -0400

    [BASE64] Implementation of the Base64 encoder -The App can code and decode the user email using Base64

---
+++ Lembrol/app/src/main/java/com/example/ana/lembrol/Config/Base64Custom.java
@@ -0,0 +1,15 @@
+package com.example.ana.lembrol.Config;
+
+import android.util.Base64;
+
+public class Base64Custom {
+
+    public static String code64Base(String text){
+        return Base64.encodeToString(text.getBytes(), Base64.DEFAULT).replaceAll("(\\n|\\r)","");
+    }
+
+    public static String decode64Base(String decodeText){
+        return new String( Base64.decode(decodeText, Base64.DEFAULT));
+    }
+
+}
