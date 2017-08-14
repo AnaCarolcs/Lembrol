6455b5dc1b2dff3b20427c53941b752cfabc5c68
Ana Carolina <anacarolcarvalho23@gmail.com>
2017-08-14 15:00:20 -0400

    [REFACTOR] Rename FirebaseConf to FirebaseConfig

--- Lembrol/app/src/main/java/com/example/ana/lembrol/Config/FirebaseConfig.java
+++ Lembrol/app/src/main/java/com/example/ana/lembrol/Config/FirebaseConfig.java
@@ -4,9 +4,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public final class FirebaseConf {
-
-    public static final class FirebaseConfig {
+public final class FirebaseConfig {
 
         private static DatabaseReference firebaseReference;
         private static FirebaseAuth authentication;
@@ -30,4 +28,3 @@ public final class FirebaseConf {
 
     }
 
-}
