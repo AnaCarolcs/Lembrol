0ef2d75627ddb2eafb4744ac6b18cff37bd23181
Ana Carolina <anacarolcarvalho23@gmail.com>
2017-08-16 00:46:33 -0400

    [ADAPTER] Class TitleAdapter created to RecyclerView -ViewHolder implemented;

--- Lembrol/app/src/main/java/com/lembrol/ana/View/TitleAdapter.java
+++ Lembrol/app/src/main/java/com/lembrol/ana/View/TitleAdapter.java
@@ -1,56 +1,54 @@
 package com.lembrol.ana.View;
 
-
-import android.content.Context;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
 import com.lembrol.ana.Model.Reminder;
-import com.lembrol.ana.Model.Title;
 import com.lembrol.ana.R;
 
 import java.util.List;
 
-public class TitleAdapter extends ArrayAdapter<Reminder> {
+public class TitleAdapter extends RecyclerView.Adapter<TitleAdapter.MyViewHolder>{
 
-    private ArrayList<Reminder> reminders;
-    private Context context;
 
-    public TitleAdapter(Context c, ArrayList<Reminder> objects) {
-        super(c, 0, objects);
-        this.reminders = objects;
-        this.context = c;
+    private List<String> titleReminder;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView titleReminder;
+
+        public MyViewHolder(View view) {
+            super(view);
+            titleReminder = (TextView) view.findViewById(R.id.tv_title);
+        }
+    }
+
+
+    public TitleAdapter(List<String> titleReminder) {
+        this.titleReminder = titleReminder;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.title_list, parent, false);
 
-        View view = null;
-
-        //Verifica se tem lista
-        if (reminders != null){
-
-            //inicializar objetos para montagem da view
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
-
-            //Montar a view a partir do xml
-            view = inflater.inflate(R.layout.title_list, parent, false);
-
-            //Recupera elemento para exibição
-            TextView titleReminder = (TextView) view.findViewById(R.id.tv_title);
-
-            Reminder reminder = reminders.get(position);
-            titleReminder.setText(reminder.getTitleReminder());
-        }
-
-        return view;
+        return new MyViewHolder(itemView);
     }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        String titleKey = titleReminder.get(position);
+        holder.titleReminder.setText(titleKey);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return titleReminder.size();
+    }
+
+
 }
